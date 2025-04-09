3b64d66b1a6627ef94b846383b833d22e4a94de0
fernando8franco <francoduranluisfernando@gmail.com>
2025-04-08 19:26:59 -0600

    program v1 finished

--- main.go
+++ main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"regexp"
+	"strings"
 
 	godiacritics "gopkg.in/Regis24GmbH/go-diacritics.v2"
 )
 
+var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9 ]+`)
+
+func clearString(str string) string {
+	return nonAlphanumericRegex.ReplaceAllString(str, "")
+}
+
 func main() {
-	noDiacrits := godiacritics.Normalize("än éᶍample")
-	fmt.Println(noDiacrits)
+	scanner := bufio.NewScanner(os.Stdin)
+	slugs := []string{}
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		if text == "" {
+			break
+		}
+
+		lowerCase := strings.ToLower(text)
+		noDiacrits := godiacritics.Normalize(lowerCase)
+		noSimbols := clearString(noDiacrits)
+		slug := strings.ReplaceAll(noSimbols, " ", "-")
+
+		slugs = append(slugs, slug)
+	}
+
+	fmt.Println(slugs)
 }
